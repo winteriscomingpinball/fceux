e01fca74e601d99ea503632076d1450c95eb964a
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-08 07:12:37 -0500

    Update types.h

--- src/types.h
+++ src/types.h
@@ -36,12 +36,12 @@
 #endif
 
 #ifdef MSVC
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef signed char int8;
-typedef signed short int16;
-typedef signed int int32;
+//typedef unsigned char uint8;
+//typedef unsigned short uint16;
+//typedef unsigned int uint32;
+//typedef signed char int8;
+//typedef signed short int16;
+//typedef signed int int32;
 #define dup _dup
 #define stat _stat
 #define fstat _fstat
